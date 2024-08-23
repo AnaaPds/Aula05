ad5ee09ea706fb49834a7c41a160394f59f52fd8
AnaaPds <anapauladantas1804@gmail.com>
2024-08-23 11:19:43 -0300

    Add files via upload

---
+++ ContaCorrente.java
@@ -0,0 +1,21 @@
+package entidade;
+
+public class ContaCorrente extends Conta{
+
+
+		private double limite;
+
+		public ContaCorrente(int numero, String nome, double saldo, double limite) {
+			super(nome, numero, saldo, limite);
+			this.limite = limite;
+		}
+
+		@Override
+		public String toString() {
+			return "ContaCorrente " + mostrar() + "limite=" + limite + "";
+		}
+		
+		
+		
+
+}
